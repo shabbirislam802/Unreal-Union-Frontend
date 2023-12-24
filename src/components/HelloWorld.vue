4955cb0f4c86be8830f53d3a89f8d64b12098692
shabbirislam802 <149245820+shabbirislam802@users.noreply.github.com>
2023-12-24 16:10:28 +0100

    Added tailwind

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -19,7 +19,7 @@ const count = ref(0)
     </p>
   </div>
 
-  <p>
+  <p class="text-3xl font-bold underline">
     Check out
     <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
       >create-vue</a
@@ -32,9 +32,3 @@ const count = ref(0)
   </p>
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
-
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
